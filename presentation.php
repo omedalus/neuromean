5090995888e09c752cec863d1b9389a8d772dcaa
Omedalus <mvol@live.com>
2017-11-10 23:43:44 +0000

    More graphics, and making the side box

--- presentation.php
+++ presentation.php
@@ -15,6 +15,7 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
     <link rel="stylesheet" type="text/css" href="style/presentation.css" />
     <link rel="stylesheet" type="text/css" href="style/neuromean-viz.css" />
     <link rel="stylesheet" type="text/css" href="style/slider.css" />
+    <link rel="stylesheet" type="text/css" href="style/scrollbar.css" />
     
     <title>Modeling odor tracking computations in the peripheral nervous system of a predatory snail</title>
   </head>
@@ -50,17 +51,29 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
       </div>
       <div class="screenbox roundedscreen">
         <label>Methods</label>
-        <img src="img/tentacle-nerve.png" id="tentaclenerveinlay"></img>
+        <video playsinline autoplay muted loop poster="img/method-qtips.jpg" id="methodqtips">
+            <source src="img/2pt-method-closeup-360p.mp4" type="video/mp4">
+        </video>
       </div>
       <div class="screenbox roundedscreen">
         <label>Observations</label>
-        <img src="img/tentacle-nerve.png" id="tentaclenerveinlay"></img>
+        <img src="img/yafremava-mean-normalized-nerve-response.gif" id="yafremavanerveresp"></img>
       </div>
+      <div class="screenbox roundedscreen">
+        <label>Model</label>
+        <img src="img/modelsnap.png" id="modelsnap"></img>
+      </div>
+    </div>
+    
+    
+    <div class="textholder roundedscreen nicescroll">
+      <p>
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+      </p>
     </div>
     
     <article class="roundedscreen" data-neuromean-viz="">
     </article>
-    
     
   </body>
 
