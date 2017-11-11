68226f79a195b745c4903bfc0b219c0fad8c798c
Omedalus <mvol@live.com>
2017-11-11 21:19:45 +0000

    Lightbox works

--- app/presentation/thumbclick.js
+++ app/presentation/thumbclick.js
@@ -3,15 +3,49 @@
 /* global app */
 
 (function() {
-  app.directive("thumbclick", [function() {
+  app.directive("thumbclick", ['$document', function($document) {
     let link = function(scope, element, attrs) {
-      console.log(scope.thumbclick)
+      let theLightbox = $('<div/>');
+      theLightbox.css({
+        background: 'rgba(0,0,0,.75)',
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        width: '100%',
+        height: '100%'
+      });
+      theLightbox.click(function() {
+        this.parentNode.removeChild(this);
+      });
+      
+      let theContent = $('<div/>');
+      theContent.css({
+        background: '#000',
+        color: '#000',
+        border: '3px solid #ffc',
+        borderRadius: '20px',
+        width: '60%',
+        position: 'absolute',
+        left: '20%',
+        top: '20%',
+        overflow: 'hidden'
+      });
+      theLightbox.append(theContent);
+      
+      element.find('[data-ng-transclude]').appendTo(theContent);
+
+      let onclick = function() {
+        $document.find('body').append(theLightbox);
+      };
+      
+      element.click(onclick);
     };
     
     
     return {
       templateUrl: 'app/presentation/thumbclick.html',
       link: link,
+      transclude: true,
       scope: {
         thumbclick: '='
       }
